2aad2717868b577198b23f35e4c039f4e588d2db
Jorge Sánchez Vega <jorge.sanchez@opinno.com>
2017-03-07 13:39:20 +0100

    Initial Commit

---
+++ wizardWithDotsTests/wizardWithDotsTests.swift
@@ -0,0 +1,36 @@
+//
+//  wizardWithDotsTests.swift
+//  wizardWithDotsTests
+//
+//  Created by Jorge Sánchez Vega on 7/3/17.
+//  Copyright © 2017 Jorge Sánchez Vega. All rights reserved.
+//
+
+import XCTest
+@testable import wizardWithDots
+
+class wizardWithDotsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
